a54b16d9d6969a05da561d5ca4141137b3745fdb
Lobkov Constantine <klobkov@ozon.ru>
2025-06-17 20:01:49 +0800

    first rolldown

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let value = $state('');
+
+	let reversed = $derived(value.split('').reverse().join(''));
+
+	type Props = {
+		submit: VoidFunction;
+	};
+
+	let props: Props = $props();
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
+
+<input bind:value />
+
+{props.submit}
+{reversed}
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig, type Plugin } from 'vite';
+
+const debugPlugin: Plugin = {
+	name: 'debug'
+	// configResolved(config) {
+	// 	 console.log(config);
+	// }
+};
+
+export default defineConfig({
+	plugins: [sveltekit(), debugPlugin]
+});
